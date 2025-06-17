481364af734d630ff25a29d275938280d8d0cc8f
sandeepD-Lotus <130129899+sandeepD-Lotus@users.noreply.github.com>
2025-06-17 16:00:55 +0530

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -17,7 +17,7 @@ const Index = () => {
       <div className="py-20 harmony-gradient relative">
         <div className="absolute inset-0">
           <img 
-            src="public/lovable-uploads/businessmen-working-strategic-planning.jpg" 
+            src="public/lovable-uploads/19449741.jpg" 
             alt="Background" 
             className="w-full h-full object-cover opacity-30"
           />
